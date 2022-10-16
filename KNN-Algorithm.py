70ecd666278e8f13bd7c6568546848433d719500
RupaliCodes <rupalisharma.du.or50@gmail.com>
2022-10-16 17:30:28 +0530

    Updating the code
    Removing the redundant comments, updated ReadMe, and updated dataset file

--- KNN-Algorithm.py
+++ KNN-Algorithm.py
@@ -4,41 +4,26 @@ from math import sqrt
 import pandas as pd
 import os
 import numpy as np
-#from sklearn.preprocessing import LabelEncoder 
   
-dataset = pd.read_csv(os.path.dirname(os.path.abspath(__file__)) + '/dataset/iris2.csv')
+dataset = pd.read_csv(os.path.dirname(os.path.abspath(__file__)) + '/dataset/iris.csv')
 dataset.drop (["Id"], axis=1, inplace = True)
 print (dataset)
 
-
-
 def eucl_dist(row1, row2):
     distance = 0.0
-    for i in range (len(row1)-1): # why the -1 ? because it does not want label column
+    for i in range (len(row1)-1): # -1 because it does not want label column
         distance += (row1[i]-row2[i])**2
     return sqrt (distance)
 
-#import the dataset here
-# test row specify
-"""
-for i in range(dataset.shape[0]):
-    distance = eucl_dist(myrow, dataset.iloc[i])
-    print (distance)
-"""
-# locate the nearest neighbours
-# row is the normal training dataset row 
-# myrow is the test row/ new row
-# num is the k in the knn, i.e. the numbers of nearest neighbour you want
-
-def get_neighbours (dataset, myrow, num):
+def get_neighbours (dataset, myInputRow, num):
     
     distan = list()
 
     for j in range(dataset.shape[0]):
-        dist = eucl_dist(dataset.iloc[j], myrow)
+        dist = eucl_dist(dataset.iloc[j], myInputRow)
         distan.append((dataset.iloc[j], dist))
 
-    distan.sort(key= lambda n : n [1]) #how does the n/tup work?
+    distan.sort(key= lambda n : n [1])
 
     neighbours = list()
 
@@ -46,28 +31,18 @@ def get_neighbours (dataset, myrow, num):
         neighbours.append(distan[i][0])
     return neighbours
 
-#neighbours  = get_neighbours( dataset, myrow, 3)
-
-#for neighbour in neighbours: 
-#    print (neighbour)
-# specify the neighbours parameters to get the result 
-
-def predict (dataset,myrow, num):
-    neighbours = get_neighbours (dataset,myrow, num)
-    output = [i[-1] for i  in neighbours] # what does this give in a form of list?
-    prediction = max(set (output), key= output.count)  # what is the role of the set function here?
+def predict (dataset,myInputRow, num):
+    neighbours = get_neighbours (dataset,myInputRow, num)
+    output = [i[-1] for i  in neighbours]
+    prediction = max(set (output), key= output.count)
     return prediction 
 
-# specify the predict parameters here
-#myrow = dataset.iloc[19]
-print ('enter the the sepal length and petal length respectively : ')
-myrow = np.zeros(4, float)
+print ('enter the the sepal length, sepal width, petal length and petal width respectively : ')
+myInputRow = np.zeros(4, float)
 for i in range (4):
-    myrow[i] = float (input ('Input the data point:'))
+    myInputRow[i] = float (input ('Input the data point:'))
 
-print(myrow)
-predict = predict( dataset, myrow, 13)
+print(myInputRow)
+predict = predict( dataset, myInputRow, 13)
 
-print('Expected %d, Got %d' % (myrow[-1], predict)) 
-# Instead of a dataset row, as in the example, if we had a single row , how would the last element of the row be represented? x[-1] ?
-
+print('This', str(myInputRow),' sample may belong to the variety: ', predict)
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# KNN-Algorithm-
-This demonstrates the functioning of KNN algorithm from scratch
+# KNN-Algorithm
+
+##This programme demostrates the functioning of K Nearest Neighbours Algorithm. 
+
+It does so from scratch, using modules like numpy, panda, math. It does not make use of the in-built knn classifier and related methods of python modules.
+
+##USAGE :
+
+Currently it is working by taking data from the Iris Dataset. It is required to classify the input sample into the 3 varieties of Iris flower (Setosa , Versicolor and Virginica), by considering the various parameters like-
+
+    1. sepal width 
+    2. sepal length 
+    3. petal width 
+    4. petal length 
+
+##METHODOLOGY:
+
+The following is a step by step explanation of what the program does:
+
+### 1: Reading the iris.csv file
+        It reads the .csv file and stores it into the dataframe called dataset
+        It drops the ID column as it is redundant, and prints the remaining column.
+
+        ```dataset = pd.read_csv(os.path.dirname(os.path.abspath(__file__)) + '/dataset/iris.csv')
+        dataset.drop (["Id"], axis=1, inplace = True)
+        print (dataset)```
+
+### 2: Asking for Input sample 
+
+        ```print ('enter the the sepal length, sepal width, petal length and petal width respectively : ')
+        myInputRow = np.zeros(4, float)
+        for i in range (4):
+        myInputRow[i] = float (input ('Input the data point:'))```
+
+### 3: Kickstarting the knn algorithm process
+
+```print(myInputRow)
+predict = predict( dataset, myInputRow, 13)```
+
+
+
